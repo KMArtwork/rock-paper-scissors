cd07f784e0511564b07c476b93d0649ac4e60b16
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-07-26 00:14:14 -0700

    finish mobile version

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,9 +7,9 @@ import Score from "../Score";
 function Header() {
 
   return(
-    <Box sx={{border: '1px solid white', borderRadius: '5px', padding: '1rem', display: 'flex', justifyContent: 
-    'space-between', alignItems: 'center'}}>
-      <img src={titleLogo} style={{width: 'auto', height: '60%'}}/>
+    <Box sx={{border: '3px solid hsl(217, 16%, 45%)', borderRadius: '5px', padding: '1rem', display: 'flex', justifyContent: 
+    'space-between', alignItems: 'center', height: '60px'}}>
+      <img src={titleLogo} style={{width: 'auto', height: '80%', marginLeft: '0.25rem'}}/>
       <Score />
     </Box>
   )
--- src/components/RulesButton/index.jsx
+++ src/components/RulesButton/index.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 
-import { Container } from "@mui/material";
+import { Container, Modal, Box, Button } from "@mui/material";
+import rulesIcon from '../../assets/images/image-rules.svg';
+import closeIcon from '../../assets/images/icon-close.svg';
 
 function RulesButton() {
 
+  const [showModal, toggleModal] = useState(false);
+
+  const handleToggle = () => {
+    toggleModal(!showModal)
+  }
+
   return(
-    <Container id='rules-button'>
-      <p>RULES</p>
-    </Container>
+    <>
+      <Container id='rules-button' onClick={handleToggle}>
+        <p>RULES</p>
+      </Container>
+
+      <Modal
+        open={showModal}
+        onClose={handleToggle}
+      >
+
+        <Box id='rules-modal'>
+          <h1 style={{color: 'navy', fontSize: '2rem', fontWeight: '700', marginTop: '2rem'}}>Rules</h1>
+          <img src={rulesIcon} alt="rules of rock-paper-scissors" />
+          <Button onClick={handleToggle}>
+            <img src={closeIcon} alt="close rules icon" />
+          </Button>
+        </Box>
+
+      </Modal>
+    </>
   )
 
 }
